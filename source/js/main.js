2d95f7d72975bf882270f6a1e6de14cbd8985aef
Илья Афанасьев < >
2021-08-19 14:27:02 +0300

    правки по замечаниям наставника

--- source/js/main.js
+++ source/js/main.js
@@ -139,10 +139,10 @@
 
     // If we need pagination
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
       },
     },
 
